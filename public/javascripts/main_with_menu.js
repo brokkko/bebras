d4a4c1a704277b33a42d1790ee984024f6e31d3f
Ilya Posov <iposov@gmail.com>
2013-10-08 13:14:12 +0400

    fix contest result appearance

--- public/javascripts/main_with_menu.js
+++ public/javascripts/main_with_menu.js
@@ -5,7 +5,6 @@ $(function () {
         var footer_height = $('footer').outerHeight();
         var content_header_height = $('.content-header').outerHeight();
         var content_footer_height = $('.content-footer').outerHeight();
-        console.log(header_height, footer_height, content_footer_height);
         //padding: 0 0, margin: 4 4, border 2 2
         $('.content.auto-size').height(window_height - content_footer_height - content_header_height - 2 - 2 - 4 - 4 - footer_height - header_height);
     };
