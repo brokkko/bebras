cb04b76d2624e6ec11006043959afed6480271b7
Ilya Posov <iposov@gmail.com>
2014-09-23 12:17:15 +0400

    fix password restore link after non successful login

--- app/models/forms/validators/AuthenticatorValidator.java
+++ app/models/forms/validators/AuthenticatorValidator.java
@@ -1,7 +1,9 @@
 package models.forms.validators;
 
+import models.Event;
 import models.User;
 import models.newserialization.FormDeserializer;
+import play.api.mvc.Call;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,20 +23,22 @@ public class AuthenticatorValidator extends Validator<FormDeserializer> {
         String login = form.readString("login"); //they are required, so we sure that we can get this info
         String password = form.readString("password");
 
+        Call remindCall = controllers.routes.Registration.passwordRemind(Event.currentId());
+
         if (password == null)
-            return message();
+            return message(remindCall.url());
 
         User user = User.getInstance(User.FIELD_LOGIN, login);
         if (user != null) {
             boolean wrongPassword = ! user.testPassword(password);
-            Boolean confirmed = user.isConfirmed();
+            boolean confirmed = user.isConfirmed();
 
-            if (wrongPassword || confirmed == null || ! confirmed)
+            if (wrongPassword || ! confirmed)
                 user = null;
         }
 
         if (user == null)
-            return message();
+            return message(remindCall.url());
 
         return data(user);
     }
