8f16a15619cb9e482af3decb1e17b272cd31e68e
Ilya Posov <iposov@gmail.com>
2017-09-10 23:40:31 +0300

    add emails export for bebras contest

---
+++ scripts/export-emails.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+event=$1
+host=$2
+
+mongoexport --host $host -d dces2 -c users -q '{"want_ad": true, "_role": "SCHOOL_ORG", "event_id": "'$event'"}' --type=csv -f name,patronymic,surname,email -o $(event)_emails.csv
