58208853fd1381f5e294c5ec13e9264f71f11b53
Ilya Posov <iposov@gmail.com>
2017-10-03 07:53:15 +0300

    fix payment view

--- scripts/export-emails.sh
+++ scripts/export-emails.sh
@@ -3,4 +3,4 @@
 event=$1
 host=$2
 
-mongoexport --host $host -d dces2 -c users -q '{"want_ad": true, "_role": "SCHOOL_ORG", "event_id": "'$event'"}' --type=csv -f name,patronymic,surname,email -o $(event)_emails.csv
+mongoexport --host $host -d dces2 -c users -q '{"want_ad": true, "_role": "SCHOOL_ORG", "event_id": "'$event'"}' --type=csv -f name,patronymic,surname,email -o ${event}_emails.csv
