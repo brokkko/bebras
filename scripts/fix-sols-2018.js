61cb480842ca59480617d3bc5f4290a9b9b57829
ilya <ilya@adams>
2018-11-28 04:04:40 +0300

    add babel polyfill to bebras dyn problem

---
+++ scripts/fix-sols-2018.js
@@ -0,0 +1,12 @@
+//Дубровская? HR-08
+db['contest-bebras18_9-10'].find({
+    u: ObjectId("5befab9698ec007cce36a996"),
+    pid: ObjectId("5bc3998c98ec00bf56194806")});
+
+db['contest_bebras18_9-10'].insert({
+    u: ObjectId("5befab9698ec007cce36a996"),
+    pid: ObjectId("5bc3998c98ec00bf56194806"),
+    a : { "r" : 2, "s" : "1110000001000001100000100000010000001101110011101" },
+    "lt" : NumberLong(2287739),
+    "st" : ISODate("2018-11-17T07:05:25.580Z")
+});
