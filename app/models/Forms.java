22eeee30f74c2c7729138c21501275aff143bdb1
Ilya Posov <iposov@gmail.com>
2020-11-23 13:40:26 +0300

    create skin for koza

--- app/models/Forms.java
+++ app/models/Forms.java
@@ -225,7 +225,7 @@ public class Forms {
                                             "type", "dropdown",
                                             "title", "Оформление",
                                             "placeholder", "Выберите тип оформления",
-                                            "variants", Utils.listify("bbtc", "bebras", "bebras-by", "kio", "issep2018", "cam19")
+                                            "variants", Utils.listify("bbtc", "bebras", "bebras-by", "kio", "issep2018", "cam19", "koza")
                                     ),
                                     "required", true
                             ),
--- scripts/create_contests_koza_20.js
+++ scripts/create_contests_koza_20.js
@@ -1,2 +1,33 @@
 var koza20 = db.events.findOne({_id: "koza20"});
+var contests = koza20.contests;
 
+for (let contest of contests) {
+    let id = contest.id;
+    contest.tables = [];
+
+    contest.rights = [];
+    if (/task2-../.test(contest.id))
+        contest.rights.push("participant~league=2");
+    if (/task5-../.test(contest.id))
+        contest.rights.push("participant~league=1");
+    if (/.*-ru/.test(contest.id))
+        contest.rights.push("participant~language=ru");
+    else
+        contest.rights.push("participant~language=en");
+
+    contest['page sizes'] = [1];
+    contest.start = new Date(2020, 11 - 1, 25, 10, 0, 0, 0);
+    contest.finish = new Date(2020, 11 - 1, 25, 12, 0, 0, 0);
+    contest.results = new Date(2020, 11 - 1, 26, 23, 0, 0, 0)
+    contest.duration = 0;
+    contest["allow restart"] = false;
+}
+
+db.events.update(
+    {_id: "koza20"},
+    {
+        $set: {
+            contests: contests
+        }
+    }
+);
